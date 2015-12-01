953048a242d5ff54fb794f67c343c3dadaf18254
Harrison Gerber <gerberhe@s.dcsdk12.org>
2015-12-01 10:22:25 -0700

    Started code for drawing repeated backgrounds

--- main.rb
+++ main.rb
@@ -12,11 +12,30 @@ class Window < Gosu::Window
 
 		@player = Player.new
 		@player.warp(width/2.0, (height/2.0) + height / 2.5)
+
+		@bg_x = 0
+		@bg_y = 0
 	end
 
 	def draw
 		@player.draw
-		@background_image.draw(0, 0, ZOrder::BACKGROUND)
+		draw_backgrounds
+	end
+
+	def draw_backgrounds
+		@backgrounds = 0
+		while @backgrounds <= 100
+			@background_image.draw(@bg_x, @bg_y, ZOrder::BACKGROUND)
+			@backgrounds += 1
+			@bg_x += 402
+			@bg_y += 402
+		end
+		while @backgrounds > 100 && @backgrounds <= 200
+			@background_image.draw(@bg_x, @bg_y, ZOrder::BACKGROUND)
+			@backgrounds += 1
+			@bg_x -= 402
+			@bg_y -= 402
+		end
 	end
 
 end
