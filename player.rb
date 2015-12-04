7b6f1d51ce6fbda7f94835c245f41251011840ea
Harrison <gerberhe11@gmail.com>
2015-12-04 06:48:38 -0700

    Made code so player can turn at smooth angles

--- player.rb
+++ player.rb
@@ -19,4 +19,64 @@ class Player
 		@image.draw_rot(@x, @y, ZOrder::PLAYER, @angle)
 	end
 
+	def direction_up
+		if @angle < 180.0
+			@angle += 10.0
+			if @angle == 180.0
+				@angle = 180.0
+			end
+		elsif @angle > 180.0
+			@angle -= 10.0
+			if @angle == 180.0
+				@angle = 180.0
+			end
+		end
+	end
+
+	def direction_right
+		if @angle < 270.0
+			@angle += 10.0
+			if @angle == 270.0
+				@angle = 270.0
+			end
+		elsif @angle > 270.0
+			@angle -= 10.0
+			if @angle == 270.0
+				@angle = 270.0
+			end
+		end
+	end
+
+	def direction_down
+		if @angle <= 180.0
+			if @angle != 0.0
+				@angle -= 10.0
+			end
+			if @angle == 0.0
+				@angle = 0.0
+			end
+		elsif @angle > 180.0
+			if @angle != 360.0
+				@angle += 10.0
+			end
+			if @angle == 360.0
+				@angle = 360.0
+			end
+		end
+	end
+
+	def direction_left
+		if @angle < 90.0
+			@angle += 10.0
+			if @angle == 90.0
+				@angle = 90.0
+			end
+		elsif @angle > 90.0
+			@angle -= 10.0
+			if @angle == 90.0
+				@angle = 90.0
+			end
+		end
+	end
+
 end
